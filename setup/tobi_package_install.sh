cdd675418fb8f1a91cf8e9e924679ac6963331da
julius59 <julius59@users.noreply.github.com>
2016-01-28 21:06:02 +0100

    Switched to tag 0.1.0

--- setup/tobi_package_install.sh
+++ setup/tobi_package_install.sh
@@ -3,6 +3,10 @@
 mkdir -p ~/catkin_ws/src
 cd ~/catkin_ws/src/
 git clone https://github.com/julius59/tobi_package.git
+cd tobi_package
+git checkout tags/0.1.0 -b 0.1.0
+
+cd ~/catkin_ws/src/
 catkin_init_workspace 
 cd ~/catkin_ws/
 catkin_make
